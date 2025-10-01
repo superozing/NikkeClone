d150fb91fd694d0074ebf5699b233a94338cbd25
superozing <amazingozing@gmail.com>
2025-10-01 09:53:03 +0900

    feat: 테스트용 UI의 View 부분 추가

---
+++ Assets/Scripts/UI/Popup/UI_PopupTest.cs
@@ -0,0 +1,33 @@
+using TMPro;
+using UI;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_PopupTest : UI_Popup
+{
+    [SerializeField] private Button _confirmButton;
+    [SerializeField] private TMP_Text _titleText;
+
+    private PopupTestViewModel _viewModel;
+
+    public override void SetViewModel(IViewModel viewModel)
+    {
+        base.SetViewModel(viewModel);
+        _viewModel = viewModel as PopupTestViewModel;
+    }
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        _confirmButton.onClick.AddListener(() => _viewModel?.OnConfirm());
+    }
+
+    protected override void OnStateChanged()
+    {
+        if (_viewModel == null) 
+            return;
+
+        _titleText.text = _viewModel.Title;
+    }
+}
