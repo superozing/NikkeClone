4a5c08f09650a18ff24e918bb42335ade952ea45
superozing <amazingozing@gmail.com>
2025-10-26 19:10:14 +0900

    refactor: View의 의존성을 줄이기 위한 IIconViewModel 수정
    sprite를 viewmodel에서 직접 로드해서 줄 수 있도록 구조를 변경했어요.
    뷰는 멍청해야 해요.

--- Assets/Scripts/UI/ViewModel/IIconViewModel.cs
+++ Assets/Scripts/UI/ViewModel/IIconViewModel.cs
@@ -1,20 +1,19 @@
 using System;
+using UnityEngine;
 
 namespace UI
 {
     public interface IIconViewModel : IViewModel
     {
         /// <summary>
-        /// 메인 아이콘 스프라이트의 Addressable 주소입니다.
+        /// 메인 아이콘 스프라이트 객체입니다.
         /// </summary>
-        string MainIconAddress { get; }
+        Sprite MainIconSprite { get; }
 
         /// <summary>
-        /// 아이콘의 희귀도(Rarity) 등급을 표시하는 프레임 스프라이트의 Addressable 주소입니다.
-        /// (예: "UI/Frames/SSR", "UI/Frames/Common")
-        /// 표시할 프레임이 없으면 null 또는 string.Empty를 반환해야 합니다.
+        /// 희귀도 프레임 스프라이트 객체입니다.
         /// </summary>
-        string RarityFrameAddress { get; }
+        Sprite RarityFrameSprite { get; }
 
         /// <summary>
         /// 아이콘 하단에 표시될 수량 텍스트입니다.
