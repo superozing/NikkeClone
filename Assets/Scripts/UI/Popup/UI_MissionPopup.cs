e180715f64f9660775a5b0635fc4cbcb3d44f696
superozing <amazingozing@gmail.com>
2025-11-14 17:40:28 +0900

    feat: UI_MissionPopup 추가
    미션 개수에 따라서 미션 슬롯을 만들고, 스크롤 뷰 컨텐츠에 자식 오브젝트로 추가해요.

---
+++ Assets/Scripts/UI/Popup/UI_MissionPopup.cs
@@ -0,0 +1,106 @@
+using UI;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.InputSystem;
+using System;
+
+public class UI_MissionPopup : UI_Popup
+{
+    public override string ActionMapKey => "UI_MissionPopup";
+
+    [Header("Components")]
+    [SerializeField] private TMP_Text _missionResetTimeText; // "HH시간 MM분 남음"
+    [SerializeField] private Transform _missionSlotRoot;
+    [SerializeField] private GameObject _missionCompleteBlockerImage;
+    [SerializeField] private TMP_Text _missionCompleteTimerText; // "HH:MM::SS"
+
+    [Header("Buttons")]
+    [SerializeField] private Button _blocker;
+    [SerializeField] private Button _exitButton;
+
+    private MissionPopupViewModel _viewModel;
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        // 1. Input Action 바인딩 (ESC 키로 닫기)
+        Managers.Input.BindAction("Close", OnEscapeAction, InputActionPhase.Performed);
+
+        // 2. Button OnClick 리스너 바인딩
+        _blocker.onClick.AddListener(OnCloseClick);
+        _exitButton.onClick.AddListener(OnCloseClick);
+    }
+
+    private void OnEscapeAction(InputAction.CallbackContext _) => OnCloseClick();
+    private void OnCloseClick() => _viewModel?.OnClose();
+
+    public override void SetViewModel(IViewModel viewModel)
+    {
+        if (_viewModel != null)
+            _viewModel.OnCloseRequested -= OnCloseRequested;
+
+        _viewModel = viewModel as MissionPopupViewModel;
+        if (_viewModel == null && viewModel != null)
+        {
+            Debug.LogError($"[UI_MissionPopup] 잘못된 ViewModel 타입이 주입되었습니다.");
+            return;
+        }
+
+        if (_viewModel != null)
+            _viewModel.OnCloseRequested += OnCloseRequested;
+
+
+        // UI_MissionSlot 생성
+        GenerateMissionSlots();
+
+        base.SetViewModel(_viewModel);
+    }
+
+    /// <summary>
+    /// ViewModel이 가진 SlotViewModels 목록을 기반으로 UI_MissionSlot을 생성합니다.
+    /// </summary>
+    private async void GenerateMissionSlots()
+    {
+        if (_viewModel == null || _missionSlotRoot == null || _viewModel.SlotViewModels == null)
+            return;
+
+        // ViewModel 리스트를 순회하며 UIManager에 생성을 요청합니다.
+        foreach (var slotViewModel in _viewModel.SlotViewModels)
+            await Managers.UI.ShowAsync<UI_MissionSlot>(slotViewModel, _missionSlotRoot);
+    }
+
+    protected override void OnStateChanged()
+    {
+        if (_viewModel == null) 
+            return;
+
+        _missionResetTimeText.text = _viewModel.MissionResetTimeText;
+        _missionCompleteTimerText.text = _viewModel.MissionCompleteTimerText;
+        _missionCompleteBlockerImage.SetActive(_viewModel.IsAllMissionsComplete);
+    }
+
+    private void OnCloseRequested()
+    {
+        // 연출 추가해야 해요
+
+        Managers.UI.Close(this);
+    }
+
+    protected override void OnDestroy()
+    {
+        base.OnDestroy();
+
+        Managers.Input.UnbindAction("Close", OnEscapeAction, InputActionPhase.Performed);
+
+        _blocker.onClick.RemoveListener(OnCloseClick);
+        _exitButton.onClick.RemoveListener(OnCloseClick);
+
+        if (_viewModel != null)
+            _viewModel.OnCloseRequested -= OnCloseRequested;
+
+        (_viewModel as IDisposable)?.Dispose();
+        _viewModel = null;
+    }
+}
