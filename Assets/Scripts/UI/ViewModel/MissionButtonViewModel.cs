272b7ddd9b13c4b556004682ae02b7c9f254e448
superozing <amazingozing@gmail.com>
2025-11-24 14:26:50 +0900

    refactor: UI_MissionButton에 reactiveproperty 사용하도록 수정

--- Assets/Scripts/UI/ViewModel/MissionButtonViewModel.cs
+++ Assets/Scripts/UI/ViewModel/MissionButtonViewModel.cs
@@ -5,19 +5,17 @@ using UnityEngine;
 
 public class MissionButtonViewModel : ViewModelBase
 {
-    public override event Action OnStateChanged;
     public event Action OnRequestMissionPopup;
 
     /// <summary>
     /// View에 표시될 현재 미션 설명 텍스트입니다.
     /// </summary>
-    public string MissionDesc { get; private set; }
+    public ReactiveProperty<string> MissionDesc { get; private set; } = new("");
 
     /// <summary>
     /// View에 표시될 빨간 점의 활성화 여부입니다.
-    /// (완료되었으나 보상받지 않은 미션이 있을 경우 true)
     /// </summary>
-    public bool IsRedDotActive { get; private set; }
+    public ReactiveProperty<bool> IsRedDotActive { get; private set; } = new(false);
 
     private readonly Dictionary<int, UserMissionData> _userMissions;
     private readonly IReadOnlyDictionary<int, MissionGameData> _missionGameData;
@@ -43,7 +41,6 @@ public class MissionButtonViewModel : ViewModelBase
     private void OnMissionStateChanged(eMissionState _)
     {
         UpdateMissionStatus();
-        OnStateChanged?.Invoke();
     }
 
     public void OnMissionButtonClicked()
@@ -51,11 +48,8 @@ public class MissionButtonViewModel : ViewModelBase
         Debug.Log("미션 버튼 클릭");
 
         // 빨간 점 비활성화
-        if (IsRedDotActive)
-        {
-            IsRedDotActive = false;
-            OnStateChanged?.Invoke();
-        }
+        if (IsRedDotActive.Value)
+            IsRedDotActive.Value = false;
 
         // 팝업 생성
         OnRequestMissionPopup?.Invoke();
@@ -63,21 +57,26 @@ public class MissionButtonViewModel : ViewModelBase
 
     private void UpdateMissionStatus()
     {
-        // 모든 미션이 완료된 경우
-        MissionDesc = "미션 완료";
+        // 기본값 설정
+        string missionDesc = "미션 완료";
+        bool isRedDotActive = false;
 
         foreach (UserMissionData userMission in _userMissions.Values)
         {
             // 보상을 받을 미션이 있다면 빨간점 활성화
             if (userMission.state.Value == eMissionState.Completed)
-                IsRedDotActive = true;
+                isRedDotActive = true;
             // 진행중인 미션이 있다면 설명 설정
             else if (userMission.state.Value == eMissionState.InProgress)
             {
                 if (_missionGameData.TryGetValue(userMission.id, out MissionGameData gameData))
-                    MissionDesc = gameData.description;
+                    missionDesc = gameData.description;
             }
         }
+
+        // 값 변경 시에만 할당 (ReactiveProperty 최적화)
+        if (MissionDesc.Value != missionDesc) MissionDesc.Value = missionDesc;
+        if (IsRedDotActive.Value != isRedDotActive) IsRedDotActive.Value = isRedDotActive;
     }
 
     protected override void OnDispose()
