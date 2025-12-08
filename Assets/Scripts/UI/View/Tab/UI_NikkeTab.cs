a2e4a1633dc8f48811aa943777ed4f13e302bc66
superozing <amazingozing@gmail.com>
2025-12-08 14:51:15 +0900

    feat: UI_NikkeTab이 자식 뷰모델 세팅

--- Assets/Scripts/UI/View/Tab/UI_NikkeTab.cs
+++ Assets/Scripts/UI/View/Tab/UI_NikkeTab.cs
@@ -5,11 +5,17 @@ public class UI_NikkeTab : UI_TabBase
 {
     public override eTabType TabType => eTabType.Nikke;
 
+    [SerializeField] private UI_NikkeCardScrollView _scrollView;
+
     private NikkeTabViewModel _viewModel;
 
     public override void SetViewModel(ViewModelBase viewModel)
     {
         _viewModel = viewModel as NikkeTabViewModel;
+
         base.SetViewModel(viewModel);
+
+        if (_viewModel != null && _scrollView != null)
+            _scrollView.SetViewModel(_viewModel.ScrollViewModel);
     }
 }
