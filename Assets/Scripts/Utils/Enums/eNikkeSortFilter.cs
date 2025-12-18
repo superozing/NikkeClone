09ed98042c3a08938e915c6c500caef58542af92
superozing <amazingozing@gmail.com>
2025-12-18 17:57:53 +0900

    feat: eNikkeSortFilter.cs 추가
    정렬, 필터링 관련 enum을 추가했어요. 문자열로 하는 것 보다 타입으로 하는 게 정렬과 필터링에서 문제가 일어날 확률이 낮음.

---
+++ Assets/Scripts/Utils/Enums/eNikkeSortFilter.cs
@@ -0,0 +1,48 @@
+
+/// <summary>
+/// 정렬 기준
+/// </summary>
+public enum eNikkeSortType
+{
+    CombatPower,
+    Level,
+}
+
+public enum eNikkeClass
+{
+    None,
+    Attacker,   // 화력형
+    Defender,   // 방어형
+    Supporter,  // 지원형
+}
+
+public enum eNikkeCode
+{
+    None,
+    Fire,       // 작열
+    Water,      // 수냉
+    Wind,       // 풍압
+    Electric,   // 전격
+    Iron,       // 철갑
+}
+
+public enum eNikkeWeapon
+{
+    None,
+    AR,
+    SMG,
+    SG,
+    SR,
+    RL,
+    MG
+}
+
+public enum eNikkeManufacturer
+{
+    None,
+    Elysion,    // 엘리시온
+    Missilis,   // 미실리스
+    Tetra,      // 테트라
+    Pilgrim,    // 필그림
+    Abnormal    // 어브노멀
+}
