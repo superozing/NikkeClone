850d59d1016cc0415642c644ca0a4a0947e23f19
superozing <amazingozing@gmail.com>
2026-02-07 01:11:31 +0900

    refactor: 사용하지 않을 멤버 제거

--- Assets/Scripts/Combat/Field/NikkeCover.cs
+++ Assets/Scripts/Combat/Field/NikkeCover.cs
@@ -1,4 +1,4 @@
-using UnityEngine;
+﻿using UnityEngine;
 
 /// <summary>
 /// 니케의 엄폐물입니다.
@@ -6,10 +6,5 @@ using UnityEngine;
 /// </summary>
 public class NikkeCover : MonoBehaviour
 {
-    [SerializeField] private float _damageReduction = 0.5f;  // 엄폐 시 데미지 감소율
-
-    /// <summary>엄폐 시 데미지 감소율 (0~1)</summary>
-    public float DamageReduction => _damageReduction;
-
     // TODO Phase 3: 엄폐물 파괴 로직 (선택적)
 }
